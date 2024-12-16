7cfb848efc16def50d74e6f719aab06cca8b2d91
Henshou <gerrardgrs@gmail.com>
2024-12-16 18:16:46 +0700

    refactor

--- src/frontend/app/_components/Navbar.tsx
+++ src/frontend/app/_components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full h-14 flex items-center justify-between px-8 z-10">
-      <p className="text-white font-extrabold font-inter text-xl">Audio Searcher</p>
+      <p className="text-white font-extrabold font-inter text-xl">Song Tuah</p>
     </nav>
   );
 };
